4351d0cca6be4359c7d6922de4fc226ff10d1842
Connor Ruggles <cruggles@iastate.edu>
2017-07-10 21:04:39 -0500

    fix pylint errors

--- setup.py
+++ setup.py
@@ -6,54 +6,72 @@ of the original shell setup script
 for my scripts repo
 """
 
-from sys import exit, argv
+from sys import argv
 import os
 import errno
 from shutil import copy, copystat, rmtree
 
 # declare useful globals
-global home
-home = os.path.expanduser('~')
+HOME = os.path.expanduser('~')
 
-global bindir
-bindir = home + "/bin"
+BINDIR = HOME + "/bin"
 
-global confdir
-confdir = home
+CONFDIR = HOME
 
-global path_add
-path_add = 1
+PATHADD = 1
 
-global path
-path = os.environ['PATH']
+PATH = os.environ['PATH']
 
 # start functions
 def error_trig(message):
+    """
+    error_trig
+
+    @param message: the message to be output
+    @returns: nothing
+    """
     print "Error:", message
     exit(1)
 
 def usage():
+    """
+    prints help
+    """
     print "Usage: ./setup.py [OPTIONS]"
-    print "Examples: ./setup.py -b", bindir
+    print "Examples: ./setup.py -b", BINDIR
     print "          ./setup.py"
     print "Options:"
-    print "-b /path/to/bin    Install dir. Default is", bindir
+    print "-b /path/to/bin    Install dir. Default is", BINDIR
     print "-h                 Print this help."
 
-def check_dir(dir):
-    full_path = os.path.abspath(dir)
+def check_dir(dir_to_check):
+    """
+    check_dir
+
+    @param dir_to_check: explains itself
+    @return: 1 if the full path to 'dir_to_check' is a dir, 0 otherwise
+    """
+    full_path = os.path.abspath(dir_to_check)
     if os.path.isdir(full_path):
         return 1
     else:
         return 0
 
-def mkdir_p(dir):
-    full_path = os.path.abspath(dir)
+def mkdir_p(dir_to_check):
+    """
+    mkdir_p
+    provides functionality of mkdir -p
+
+    @param dir_to_check: the directory to check and see if we should make it or not
+    @return: 0 if any errors occurred while making directory, 1 if directory is made without error,
+    and exits if it was chosen not to make the directory
+    """
+    full_path = os.path.abspath(dir_to_check)
     if os.path.exists(full_path) and os.path.isdir(full_path):
         return 1
 
     while True:
-        feedback = raw_input(dir + " doesn't exist, create it? [Y/n] ")
+        feedback = raw_input(dir_to_check + " doesn't exist, create it? [Y/n] ")
         if feedback == 'Y' or feedback == 'y':
             try:
                 os.makedirs(full_path)
@@ -71,24 +89,37 @@ def mkdir_p(dir):
             print "Please enter Y(y) or n(N)"
 
 def path_check(set_dir):
+    """
+    path_check
+    checks if BINDIR is in your $PATH
+
+    @param set_dir: directory to search for in PATH
+    @return: nothing, just prints useful messages to the console
+    """
     set_dir = os.path.abspath(set_dir)
-    if set_dir not in path:
+    if set_dir not in PATH:
         if mkdir_p(set_dir):
             print "adding", set_dir, "to PATH..."
-            with open(home + "/.profile", "a+") as profile:
-                profile.write("export PATH=\"" + path + ":" + set_dir + "\"")
+            with open(HOME + "/.profile", "a+") as profile:
+                profile.write("export PATH=\"" + PATH + ":" + set_dir + "\"")
 
-            print "In order for the scripts to be readily available, you must source '$HOME/.profile'."
+            print "For the scripts to be readily available, you must source '$HOME/.profile'."
         else:
             error_trig("Something went wrong while making directory.")
     else:
         print set_dir, "already exists in PATH"
 
-    global bindir
-    bindir = set_dir
+    global BINDIR
+    BINDIR = set_dir
 
 def get_args(arg_list):
-    set_dir = bindir
+    """
+    get_args
+
+    @param arg_list: list of arguments to the script
+    @return: nothing, just prints messages if necessary
+    """
+    set_dir = BINDIR
     if len(arg_list) > 0:
         if "-h" in arg_list:
             usage()
@@ -106,38 +137,54 @@ def get_args(arg_list):
     path_check(set_dir)
 
 def force_symlink(source, dest):
+    """
+    force_symlink
+    tries to force to make a symbolic link from source to dest
+
+    @param source: hard copy of file/directory
+    @param dest: destination of link
+    """
     try:
         os.symlink(source, dest)
-    except OSError, e:
-        if e.errno == errno.EEXIST:
-            if os.isdir(dest):
+    except OSError, exc:
+        if exc.errno == errno.EEXIST:
+            if os.path.isdir(dest):
                 rmtree(dest)
             else:
                 os.remove(dest)
             os.symlink(source, dest)
 
 def install_scripts():
+    """
+    installs the scripts
+    """
     print "installing scripts..."
-    
+
     all_scripts = os.listdir("./scripts")
     os.chdir("scripts")
     for script in all_scripts:
-        copy(script, bindir)
-        copystat(script, bindir)
+        copy(script, BINDIR)
+        copystat(script, BINDIR)
 
 def install_configs():
+    """
+    installs the config files
+    """
     print "linking configs..."
-    force_symlink(os.abspath(".vim"), confdir + "/.vim")
-    force_symlink(os.abspath("cava"), confdir + "/.config/cava")
-    force_symlink(os.abspath(".bash_aliases"), confdir + "/.bash_aliases")
-    force_symlink(os.abspath(".bashrc"), confdir + "/.bashrc")
-    force_symlink(os.abspath(".profile"), confdir + "/.profile")
-    force_symlink(os.abspath(".tmux.conf"), confdir + "/.tmux.conf")
-    force_symlink(os.abspath(".xinitrc"), confdir + "/.xinitrc")
+    force_symlink(os.path.abspath(".vim"), CONFDIR + "/.vim")
+    force_symlink(os.path.abspath("cava"), CONFDIR + "/.config/cava")
+    force_symlink(os.path.abspath(".bash_aliases"), CONFDIR + "/.bash_aliases")
+    force_symlink(os.path.abspath(".bashrc"), CONFDIR + "/.bashrc")
+    force_symlink(os.path.abspath(".profile"), CONFDIR + "/.profile")
+    force_symlink(os.path.abspath(".tmux.conf"), CONFDIR + "/.tmux.conf")
+    force_symlink(os.path.abspath(".xinitrc"), CONFDIR + "/.xinitrc")
 
 def main():
+    """
+    main function
+    """
     get_args(argv[1:])
-    
+
     # passed all the checks, go ahead and install scripts
     install_scripts()
     install_configs()
