d745276ed0caedebd42365f3552d8f1f5c695659
Connor Ruggles <cruggles@iastate.edu>
2017-07-19 20:46:55 -0500

    fix pylint errors

--- scripts/backup.py
+++ scripts/backup.py
@@ -1,35 +1,37 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
+"""
+this script is a wrapper for using
+rsync, and simplifies it down to use
+the options that are commonly used
+by me.
+"""
 
-from sys import exit, argv
+from sys import argv
 import os
 import errno
 
-global SYNC_OPTS
 SYNC_OPTS = "-urpq"
 
-global ERR_FILE
 ERR_FILE = "backup_errs.log"
 
-global SRC
 SRC = ""
 
-global DEST
 DEST = ""
 
 def error_trig(message):
-    print "Error:", message
+    print("Error:", message)
     exit(1)
 
 def usage():
-    print "Usage: backup.py [OPTIONS]"
-    print "Examples:         backup.py -e err_file.txt -v"
-    print "                  backup"
-    print "Options:"
-    print "-s, --src /src/dir            Source directory."
-    print "-d, --dest /dest/dir          Destination directory."
-    print "-h, --help                    Print this help."
-    print "-e, --err_file err_file.txt   Output errors to the provided file."
-    print "-v, --verbose                 Turns on verbose logging."
+    print("Usage: backup.py [OPTIONS]")
+    print("Examples:         backup.py -e err_file.txt -v")
+    print("                  backup")
+    print("Options:")
+    print("-s, --src /src/dir            Source directory.")
+    print("-d, --dest /dest/dir          Destination directory.")
+    print("-h, --help                    Print this help.")
+    print("-e, --err_file err_file.txt   Output errors to the provided file.")
+    print("-v, --verbose                 Turns on verbose logging.")
 
 def check_dir(dir):
     full_path = os.path.abspath(dir)
@@ -40,12 +42,12 @@ def check_dir(dir):
 
 def do_backup_wo_args():
     #TODO
-    print "hi"
+    print("hi")
 
 def get_args(arg_list):
     check_dir = SRC
     err_check = ERR_FILE
-    if len(arg_list) > 0:
+    if arg_list:
         if "-h" or "--help" in arg_list:
             usage()
             exit(1)
@@ -54,13 +56,15 @@ def get_args(arg_list):
             if len(arg_list) > spos:
                 check_dir = arg_list[spos + 1]
             else:
-                error_trig("no path specified after '-s' option. Run with '-h' or '--help' for help")
+                error_trig("no path specified after '-s' option. Run with \
+                        '-h' or '--help' for help")
         if "--src" in arg_list:
             spos = arg_list.index("--src")
             if len(arg_list) > spos:
                 check_dir = arg_list[spos + 1]
             else:
-                error_trig("no path specified after '--src' option. Run with '-h' or '--help' for help")
+                error_trig("no path specified after '--src' option. Run with \
+                        '-h' or '--help' for help")
         if "-v" in arg_list:
             global SYNC_OPTS
             SYNC_OPTS = SYNC_OPTS.replace("q", "v")
@@ -72,13 +76,15 @@ def get_args(arg_list):
             if len(arg_list) > epos:
                 err_check = arg_list[epos + 1]
             else:
-                error_trig("no filename specified after '-e' option. Run with '-h' or '--help' for help")
+                error_trig("no filename specified after '-e' option. Run \
+                        with '-h' or '--help' for help")
         if "--err_file" in arg_list:
             epos = arg_list.index("--err_file")
             if len(arg_list) > epos:
                 err_check = arg_list[epos + 1]
             else:
-                error_trig("no filename specfied after '--err_file' option. Run with '-h' or '--help' for help")
+                error_trig("no filename specfied after '--err_file' option. \
+                        Run with '-h' or '--help' for help")
     else:
         do_backup_wo_args()
 
